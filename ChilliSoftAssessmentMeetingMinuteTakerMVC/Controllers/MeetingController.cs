b6a501dd8c770d772d3a11b06a093f1b843a2e67
KovlynR <kovlyn.reddy@gmail.com>
2023-01-14 20:01:46 +0200

    CleanUp

--- ChilliSoftAssessmentMeetingMinuteTakerMVC/Controllers/MeetingController.cs
+++ ChilliSoftAssessmentMeetingMinuteTakerMVC/Controllers/MeetingController.cs
@@ -18,152 +18,19 @@ public class MeetingController : Controller
     {
         _hub = hub;
     }
-    // GET: MeetingController
-    public async Task<ActionResult> Index()
-    {
-        #region FetchFromAPI
 
-        var model = new MeetingViewModel();
-        var Users = await UserService.GetAll();
-        var AllMessages = await MeetingMinutesService.GetAll();
-        var AllMeetingItems = await MeetingItemService.GetAll();
-        var AllMeetings = await MeetingService.GetAll();
-        var AllStatuses = await MeetingItemService.GetAllStatuses();
-        var AllTypes = await MeetingService.GetAllTypes();
 
-        #endregion
-
-        #region ItemDToToVM
-        var ItemVMs = new List<MeetingItemVeiwModel>();
-        foreach (var item in AllMeetingItems)
-        {
-            var entity = new MeetingItemVeiwModel()
-            {
-                ItemName = item.ItemName,
-                CreationDateTime = item.CreationDateTime,
-                StatusId = item.StatusId,
-                Comment = item.Comment,
-                CreatorId = item.CreatorId,
-                MeetingId = item.MeetingId,
-                UserId = item.UserId,
-                Id = item.Id,
-                Statuses = AllStatuses
-            };
-            ItemVMs.Add(entity);
-        }
-
-        #endregion
-
-        #region MessageDToToVM
-        var MessageVMs = new List<MeetingMessageViewModel>();
-        foreach (var item in AllMessages)
-        {
-            var entity = new MeetingMessageViewModel()
-            {
-                Message = item.Message,
-                CreationDateTime = item.CreationDateTime,
-                ItemId = item.ItemId,
-                SenderId = item.CreatorId,
-                MeetingId = item.MeetingId,
-                CreatorId = item.UserId,
-                Id = item.Id
-            };
-            MessageVMs.Add(entity);
-        }
-
-        #endregion
-
-        #region MeetingDToToVM
-
-        var MeetingVMs = new List<MeetingHeaderViewModel>();
-        foreach (var item in AllMeetings)
-        {
-            var entity = new MeetingHeaderViewModel()
-            {
-                MeetingName = item.MeetingName,
-                CreationDateTime = item.CreationDateTime,
-                CreatorId = item.CreatorId,
-                MeetingStatus = item.MeetingStatus,
-                MeetingTypeId = item.MeetingTypeId,
-                Id = item.Id,
-                AllTypes = AllTypes
-            };
-            MeetingVMs.Add(entity);
-        }
-
-        #endregion
-
-        var MyProfile = Users.FirstOrDefault(m=>m.UserEmail==User.Identity.Name);
-
-        var newMessage = new SendMeetingMessageViewModel()
-        {
-            SenderId=MyProfile.Id
-        };
-
-        model.newMeeting = new CreateMeetingViewModel() { MeetingTypes = AllTypes};
-        model.newItem = new CreateMeetingItemViewModel() { Meetings = MeetingVMs , Statuses=AllStatuses} ;
-        model.NewMessage = newMessage ;
-        model.OtherMeetings = MeetingVMs;
-        model.Messages = MessageVMs;
-        model.Items = ItemVMs;
- 
-
-        return View(model);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> SendMeetingMessage(SendMeetingMessageViewModel model) {
-
-        await MeetingMinutesService.Create(model);
-
-        var SRMessage = new SignalRMessage() { 
-        Message = model.Message,
-        UserId = model.SenderId,
-        ItemId = model.ItemId,
-        MeetingId = model.MeetingId
-        };
-
-        await SendAMessage(SRMessage);
-
-        return RedirectToAction("Index");
-    }
-
-    public async Task<IActionResult> SendAMessage(SignalRMessage Message) {
-        await _hub.Clients.All.SendAsync("RecieveMessage",Message);
-        return View();
-    }
-
-    public IActionResult OnGoingMeeting() {
-
-        return View();
-    }
-
-    public async Task<IActionResult> CreateNewMeeting(CreateMeetingViewModel newMeeting) {
-        var Users =  await UserService.GetAll();
-        var MyProfile = Users.FirstOrDefault(m => m.UserEmail == User.Identity.Name);
-        newMeeting.CreatorId = MyProfile.Id;
-        MeetingService.Create(newMeeting);
-
-        var SRMessage = new SignalRMessage()
-        {
-            Code = 101
-        };
-
-        await SendAMessage(SRMessage);
-
-        return RedirectToAction("Index");
-    }
-
-    public async Task<IActionResult> AttendMeeting(string Id) {
+    public async Task<MeetingViewModel> AssembleMeetingModel(int meetingId) {
         int MeetingId;
 
-        if (string.IsNullOrEmpty(Id))
+        if (meetingId < 1)
         {
-            return RedirectToAction("Index");
+            MeetingId = 0;
         }
-        else { 
+        else
+        {
 
-        MeetingId = Convert.ToInt32(Id);
+            MeetingId = meetingId;
         }
         #region FetchFromAPI
 
@@ -177,8 +44,6 @@ public class MeetingController : Controller
 
 
         #endregion
-
-
 
         #region MessageDToToVM
         var MessageVMs = new List<MeetingMessageViewModel>();
@@ -219,6 +84,11 @@ public class MeetingController : Controller
 
         #endregion
         var currentMeeting = MeetingVMs.FirstOrDefault(m => m.Id == MeetingId);
+        if (currentMeeting == null) { 
+            currentMeeting = new MeetingHeaderViewModel(); 
+        if (AllMeetingItems.Count > 0 ) { currentMeeting.MeetingTypeId = AllMeetingItems.First().MeetingId; }
+        }
+
         #region ItemDToToVM
         var ItemVMs = new List<MeetingItemVeiwModel>();
         foreach (var item in AllMeetingItems)
@@ -259,75 +129,66 @@ public class MeetingController : Controller
         model.MeetingName = currentMeeting.MeetingName;
         model.MeetingTypeId = currentMeeting.MeetingTypeId;
 
-        return View("Index",model);
+        return(model);
     }
 
-    // GET: MeetingController/Details/5
-    public ActionResult Details(int id)
+    // GET: MeetingController
+    public async Task<ActionResult> Index()
     {
-        return View();
-    }
+        var model = await AssembleMeetingModel(0);
 
-    // GET: MeetingController/Create
-    public ActionResult Create()
+        return View(model);
+    }
+    public async Task<IActionResult> AttendMeeting(string Id)
     {
-        return View();
+
+        var model = await AssembleMeetingModel(Convert.ToInt32(Id));
+
+        return View("Index", model);
     }
 
-    // POST: MeetingController/Create
     [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-        try
-        {
-            return RedirectToAction(nameof(Index));
-        }
-        catch
-        {
-            return View();
-        }
+    public async Task<IActionResult> SendMeetingMessage(SendMeetingMessageViewModel model) {
+
+        await MeetingMinutesService.Create(model);
+
+        var SRMessage = new SignalRMessage() { 
+        Message = model.Message,
+        UserId = model.SenderId,
+        ItemId = model.ItemId,
+        MeetingId = model.MeetingId
+        };
+
+        await SendAMessage(SRMessage);
+
+        return RedirectToAction("AttendMeeting",model.MeetingId);
     }
 
-    // GET: MeetingController/Edit/5
-    public ActionResult Edit(int id)
-    {
+    public async Task<IActionResult> SendAMessage(SignalRMessage Message) {
+        await _hub.Clients.All.SendAsync("RecieveMessage",Message);
         return View();
     }
 
-    // POST: MeetingController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-        try
-        {
-            return RedirectToAction(nameof(Index));
-        }
-        catch
-        {
-            return View();
-        }
-    }
+    public IActionResult OnGoingMeeting() {
 
-    // GET: MeetingController/Delete/5
-    public ActionResult Delete(int id)
-    {
         return View();
     }
 
-    // POST: MeetingController/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-        try
+    public async Task<IActionResult> CreateNewMeeting(CreateMeetingViewModel newMeeting) {
+        var Users =  await UserService.GetAll();
+        var MyProfile = Users.FirstOrDefault(m => m.UserEmail == User.Identity.Name);
+        newMeeting.CreatorId = MyProfile.Id;
+        MeetingService.Create(newMeeting);
+
+        var SRMessage = new SignalRMessage()
         {
-            return RedirectToAction(nameof(Index));
-        }
-        catch
-        {
-            return View();
-        }
+            Code = 101
+        };
+
+        await SendAMessage(SRMessage);
+
+        return RedirectToAction("Index");
     }
+
+
 }
